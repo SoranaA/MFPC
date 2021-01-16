11a36cfd80b54c60afbff76974a6cf7c157b3d0b
SoranaC <andreea.sorana18@yahoo.com>
2021-01-16 19:16:02 +0200

    Add project files.

---
+++ MFPC-server/Data/_DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MFPC_server.Data
+{
+    public class _DbContext : DbContext
+    {
+        public  _DbContext(DbContextOptions<_DbContext> options) : base(options) { }
+
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
